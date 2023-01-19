8015606f95fa7422f0e5bbeb35ffd00aff9504eb
mohammedmishalk <mishalnunu@gmail.com>
2023-01-19 11:27:05 +0530

    To do list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function ToDoApp() {
+
+  const emojis = ['😀', '😎', '😴', '🤔', '🤩', '🥳', '🤯', '😷', '🥶'];
+
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTasks([...tasks, newTask]);
+    setNewTask('');
+  }
+
+  const handleDelete = (index) => {
+    const updatedTasks = tasks.filter((task, i) => i !== index);
+    setTasks(updatedTasks);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainContainer">
+      <h1 className="title">To-Do App</h1>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          placeholder="Add a new task" 
+          value={newTask} 
+          onChange={e => setNewTask(e.target.value)}
+          className="inputField"
+        />
+        <button type="submit" className="addButton">Add</button>
+      </form>
+      <ul className="taskList">
+        {tasks.map((task, index) => (
+          <li key={index} className="taskItem">
+            <span className="taskText">{emojis[Math.floor(Math.random() * emojis.length)]}{task}</span>
+            <button onClick={() => handleDelete(index)} className="deleteButton">X</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default ToDoApp;
